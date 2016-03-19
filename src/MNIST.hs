d7390264c9c962f1d0c27647d191c15a0afaca2c
Caitlin Wilks <cat@cat.bio>
2016-03-19 23:16:56 +0000

    added loader for NMIST image and label files and sdl debug window

---
+++ src/MNIST.hs
@@ -0,0 +1,74 @@
+module MNIST
+  ( readMNISTImages
+  , readMNISTLabels
+  , loadTextureMNIST
+  )
+where
+
+import Control.Exception
+import qualified Data.ByteString.Lazy as BS
+import Data.List.Split (chunksOf)
+import Data.Binary.Get
+import Foreign
+import Foreign.C.Types
+import SDL
+import qualified Linear
+
+-- Reads an MNIST data file into image data.
+-- Returns the number of images, width, height, and a list of images, each of which is a list of CUChars.
+readMNISTImages :: FilePath -> IO (Int, Int, Int, [[CUChar]])
+readMNISTImages filename = do
+  file <- BS.readFile filename
+
+  let (magicNumber, numImages, numRows, numColumns) =
+        flip runGet file $ do
+          magicNumber <- getWord32be
+          numImages <- getWord32be
+          numRows <- getWord32be
+          numColumns <- getWord32be
+          return (magicNumber, numImages, numRows, numColumns)
+
+  -- Ensure the magic number is 2051. It's some combination of the bytes: 0x00, 0x00, 0x08, 0x03
+  -- Which are, in order, zero, zero, the data type (unsigned char), and the number of elements (3)
+  assert (magicNumber == 2051) (return ())
+
+  -- Read rest of file
+  let rest = map CUChar . BS.unpack . BS.drop 16 $ file
+  let images = chunksOf (fromIntegral (numRows * numColumns)) rest
+
+  return (fromIntegral numImages, fromIntegral numRows, fromIntegral numColumns, images)
+
+-- Load an nmist label file
+readMNISTLabels :: FilePath -> IO (Int, [Int])
+readMNISTLabels filename = do
+  file <- BS.readFile filename
+
+  let (magicNumber, numLabels) = flip runGet file $ do
+      magicNumber <- getWord32be
+      numLabels <- getWord32be
+      return (magicNumber, numLabels)
+
+  assert (magicNumber == 2049) (return ())
+
+  -- Read labels
+  let labels = BS.unpack . BS.drop 8 $ file
+
+  return (fromIntegral numLabels, map fromIntegral labels)
+
+-- Load an nmist image obtained from readMNISTImages into an SDL texture
+loadTextureMNIST :: SDL.Renderer -> Int -> Int -> [CUChar] -> IO SDL.Texture
+loadTextureMNIST renderer w h buf = do
+  texture <- createTexture renderer RGBA8888 TextureAccessStreaming (Linear.V2 (fromIntegral w) (fromIntegral h))
+
+  -- MNist image data is 1 byte, so duplicate each byte 4 times for 32-bit image format
+  -- (RGB888 is 32 bit)
+  let imageData = concatMap (replicate 4) buf
+
+  (ptr, pitch) <- lockTexture texture Nothing
+  let arr = castPtr ptr :: Ptr CUChar
+
+  pokeArray arr imageData
+
+  unlockTexture texture
+
+  return texture
--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import Prelude hiding (readFile, drop)
 import Control.Monad
+import Control.Monad.IO.Class
 import System.Random
 import Numeric.LinearAlgebra
 import Numeric.LinearAlgebra.Data
+import SDL
+import qualified Linear
+
 import Neural
+import MNIST
 
 -- Generates a layer with a given number of neurons with inputCount weights.
 -- Weights are randomised between 0 and 1.
@@ -23,6 +31,7 @@ genNetwork layers inputCount = sequence . map (uncurry genLayer) $ layerDefs
     -- Layer definitions in the form (neuronCount, inputCount)
     layerDefs = zip layers (inputCount:layers)
 
+-- A test network
 testNetwork :: IO Network
 testNetwork = do
   l1 <- genLayer 5 3
@@ -30,12 +39,70 @@ testNetwork = do
   l3 <- genLayer 1 5
   return [l1, l2, l3]
 
+-- Another test network
 testNetwork2 :: Network
 testNetwork2 = [hiddenLayer, outputLayer]
   where
     hiddenLayer = Layer (matrix 2 [0.15, 0.2, 0.25, 0.3]) (vector [0.35, 0.35])
     outputLayer = Layer (matrix 2 [0.4, 0.45, 0.5, 0.55]) (vector [0.6, 0.6])
 
+-- Test inputs for testNetwork2
 testInputs = vector [0.05, 0.1]
 
+-- Training data for testInputs
 trainingData = vector [0.01, 0.99]
+
+-- Train neural network
+trainNet = do
+  (imageCount, width, height, imageData) <- readMNISTImages "train-images-idx3-ubyte"
+  (labelCount, labels) <- readMNISTLabels "train-labels-idx1-ubyte"
+
+  let image = head imageData
+  let input = vector (map fromIntegral image)
+  let training = head labels
+
+  hiddenLayer <- genLayer 20 (width * height)
+  outputLayer <- genLayer 10 20
+
+  let network = [hiddenLayer, outputLayer]
+
+  let a = evalNetwork network input
+  print a
+
+  return a
+
+-- Make a window and show MNIST image
+window = do
+  initializeAll
+
+  window <- createWindow "SDL Application" defaultWindow
+  renderer <- createRenderer window (-1) defaultRenderer
+
+  -- Read in mnist data
+  (imageCount, width, height, imageData) <- readMNISTImages "train-images-idx3-ubyte"
+  (labelCount, labels) <- readMNISTLabels "train-labels-idx1-ubyte"
+
+  -- Convert to sdl texture
+  texture <- loadTextureMNIST renderer width height $ head imageData
+
+  SDL.rendererDrawColor renderer $= Linear.V4 255 255 255 255
+  SDL.clear renderer
+
+  copy renderer texture Nothing Nothing
+
+  present renderer
+
+  let loop = do
+      pollEvents
+  
+      keyState <- getKeyboardState
+
+      case keyState ScancodeEscape of
+        True -> (return ()) :: IO ()
+        False -> loop
+
+  loop
+
+  destroyTexture texture
+  destroyRenderer renderer
+  destroyWindow window
